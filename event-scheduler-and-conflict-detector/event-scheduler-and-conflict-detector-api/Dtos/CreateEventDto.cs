604157fe2ab29f2abbaa102f98c5ae4c58920888
Horla96 <sexysulai@gmail.com>
2025-08-22 11:11:57 +0100

    Add CreateEventDto and UpdateEventDto to Dtos Folder

---
+++ event-scheduler-and-conflict-detector/event-scheduler-and-conflict-detector-api/Dtos/CreateEventDto.cs
@@ -0,0 +1,16 @@
+﻿using event_scheduler_and_conflict_detector_api.Enums;
+
+namespace event_scheduler_and_conflict_detector_api.Dtos
+{
+    public class CreateEventDto
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string? Location { get; set; }
+        public List<string>? Attendees { get; set; }
+        public EventType EventType { get; set; }
+    }
+}
+}
